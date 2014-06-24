8638a87ee4f10c0771c48fb156b620b9c7973996
Sam Kerr <Sam@sam-laptop.(none)>
2014-06-24 19:45:57 -0400

    advanced controllers

--- app/controllers/customersControllerWithAs.js
+++ app/controllers/customersControllerWithAs.js
@@ -1,9 +1,44 @@
-function CustomersController(){
-	this.sortBy='name'
-	this.reverse=false;
-	this.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
-	this.doSort = function(propName){
-		this.sortBy=propName;
-		this.reverse=!this.reverse
-	}
-}
+//Option 1
+// app.controller('CustomersController',functions($scope){
+// 	$scope.sortBy='name'
+// 	$scope.reverse=false;
+// 	$scope.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
+// 	$scope.doSort = function(propName){
+// 		$scope.sortBy=propName;
+// 		$scope.reverse=!$scope.reverse
+// 	}
+// });
+
+//Option 2
+// (function() {
+// angular.module('customersApp')
+// 	.controller('CustomersController', function($scope){
+// 		$scope.sortBy='name'
+// 		$scope.reverse=false;
+		
+// 		$scope.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
+// 		$scope.doSort = function(propName){
+// 			$scope.sortBy=propName;
+// 			$scope.reverse=!$scope.reverse
+// 		}
+// 	});
+// }());
+
+//Option 3
+(function() {
+	var CustomersController = function($scope) {
+		$scope.sortBy='name'
+		$scope.reverse=false;
+		
+		$scope.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
+		$scope.doSort = function(propName){
+			$scope.sortBy=propName;
+			$scope.reverse=!$scope.reverse
+		}
+	};
+
+	CustomersController.$inject = ['$scope'];
+
+	angular.module('customersApp')
+		.controller('CustomersController', CustomersController);
+}());
