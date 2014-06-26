f7be2a09c481545a7ad3c8cfdcc0853953fba1b1
Sam Kerr <Sam@sam-laptop.(none)>
2014-06-26 16:28:26 -0400

    added multiple routes

--- app/app.js
+++ app/app.js
@@ -8,6 +8,11 @@
 				controller: 'CustomersController',
 				templateUrl: 'app/views/customers.html'
 			})
+			.when('/orders/:customerId', 
+			{
+				controller: 'OrdersController',
+				templateUrl: 'app/views/orders.html'
+			})
 			.otherwise( {redirectTo: '/'});
 		});
 }());
--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -3,8 +3,70 @@
 		$scope.sortBy='name'
 		$scope.reverse=false;
 		
-		$scope.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
-		$scope.doSort = function(propName){
+$scope.customers= [
+            {
+                id: 1, 
+                joined: '2000-12-02', 
+                name:'John', 
+                city:'Chandler', 
+                orderTotal: 9.9956,
+                orders: [
+                    {
+                        id: 1,
+                        product: 'Shoes',
+                        total: 9.9956
+                    }
+                ]
+            }, 
+            {
+                id: 2, 
+                joined: '1965-01-25',
+                name:'Zed', 
+                city:'Las Vegas', 
+                orderTotal: 19.99,
+                orders: [
+                    {
+                        id: 2,
+                        product: 'Baseball',
+                        total: 9.995
+                    },
+                    {
+                        id: 3,
+                        product: 'Bat',
+                        total: 9.995
+                    }
+                ]
+            },
+            {
+                id: 3, 
+                joined: '1944-06-15',
+                name:'Tina', 
+                city:'New York', 
+                orderTotal:44.99,
+                orders: [
+                    {
+                        id: 4,
+                        product: 'Headphones',
+                        total: 44.99
+                    }
+                ]
+            }, 
+            {
+                id: 4, 
+                joined: '1995-03-28',
+                name:'Dave', 
+                city:'Seattle', 
+                orderTotal:101.50,
+                orders: [
+                    {
+                        id: 5,
+                        product: 'Kindle',
+                        total: 101.50
+                    }
+                ]
+            }
+        ];
+    	$scope.doSort = function(propName){
 			$scope.sortBy=propName;
 			$scope.reverse=!$scope.reverse
 		}
