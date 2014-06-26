4e817300204eab5658ccc34e57eec3a1ebe78716
Sam Kerr <Sam@sam-laptop.(none)>
2014-06-26 12:48:24 -0400

    reorganized scripts and added routing

--- app/app.js
+++ app/app.js
@@ -1,3 +1,13 @@
 (function() {
-	var app = angular.module('customersApp', []);
+	var app = angular.module('customersApp', ['ngRoute']);
+
+	app.config(function($routeProvider){ 
+		$routeProvider
+			.when('/', 
+			{
+				controller: 'CustomersController',
+				templateUrl: 'app/views/customers.html'
+			})
+			.otherwise( {redirectTo: '/'});
+		});
 }());
--- app/controllers/customersController.js
+++ app/controllers/customersController.js
@@ -1,9 +1,17 @@
-function CustomersController($scope){
-	$scope.sortBy='name'
-	$scope.reverse=false;
-	$scope.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
-	$scope.doSort = function(propName){
-		$scope.sortBy=propName;
-		$scope.reverse=!$scope.reverse
-	}
-}
+(function() {
+	var CustomersController = function($scope) {
+		$scope.sortBy='name'
+		$scope.reverse=false;
+		
+		$scope.customers=[{joined: '2000-12-02', name:'John', city:'Philadelphia', orderTotal: 12.5096},{joined: '2000-09-12', name:'Sam', city:'Washington', orderTotal: 96.02},{joined: '2010-02-02', name:'Dan', city:'Seattle', orderTotal: 55.6523},{joined: '1999-11-22', name:'Ryan', city:'Frederick', orderTotal: 8}];
+		$scope.doSort = function(propName){
+			$scope.sortBy=propName;
+			$scope.reverse=!$scope.reverse
+		}
+	};
+
+	CustomersController.$inject = ['$scope'];
+
+	angular.module('customersApp')
+		.controller('CustomersController', CustomersController);
+}());
